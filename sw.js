b11918a119dd2fba9fd270483eba977452f22dfd
RB14 <RB14@users.noreply.github.com>
2016-02-16 01:26:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,16 +8,18 @@ self.addEventListener('fetch', e => {
     let count = 0;
     let stream = new ReadableStream({
         pull(cntrl) {
+            console.log("pull called");
             count++;
+
+            cntrl.enqueue(count + "");
+
             if (count > 1000) {
                 cntrl.close();
             }
-
-            cntrl.enqueue(count);
         }
     }) ;
 
-    let resp = new Response(stream, {
+    let resp = Response(stream, {
         headers: {'Content-Type': 'text/plain'}
     })
 
