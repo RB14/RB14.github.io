ab762e5e479ef9bba863b25b819da782cd0b5984
RB14 <RB14@users.noreply.github.com>
2016-02-16 01:05:49 +0200

    Create index18.js

---
+++ index18.js
@@ -0,0 +1,24 @@
+self.addEventListener('fetch', e => {
+
+    let url = e.request.url;
+    if (!url.endsWith("download")) return;
+
+    console.log(url);
+
+    let count = 0;
+    let stream = new ReadableStream({
+        pull(cntrl) {
+            count++;
+            if (count > 1000) {
+                cntrl.close();
+            }
+
+            cntrl.enqueue(count);
+        }
+    }) ;
+
+    return new Response(stream, {
+        headers: {'Content-Type': 'text/plain'}
+    })
+
+});
